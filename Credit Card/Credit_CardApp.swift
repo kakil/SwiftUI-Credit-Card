f61b5ad15978fb12131d1ce8c4089c1f8e9b803d
Kitwana Akil <kakil@me.com>
2023-05-22 11:27:24 -0400

    Initial Commit

---
+++ Credit Card/Credit_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Credit_CardApp.swift
+//  Credit Card
+//
+//  Created by Kitwana Akil on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Credit_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
